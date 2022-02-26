5d64f3f6986d17d89ef5e257199e056ac776cfff
jwyce <wycejared@gmail.com>
2022-02-26 12:35:05 -0500

    ⏪️ Revert changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-	plugins: mode === 'development' ? [react()] : [],
-	esbuild: {
-		jsxInject: `import * as React from 'react'`,
-	},
+export default defineConfig({
+	plugins: [react({ jsxRuntime: 'classic' })],
 	define: {
 		'process.env': {},
 	},
-}));
+});

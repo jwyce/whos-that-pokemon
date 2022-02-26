3b3e39e6f66005a0304bbc14ba02d9faa180bdb0
jwyce <wycejared@gmail.com>
2022-02-26 12:33:37 -0500

    🩹 Update vite config for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react({ jsxRuntime: 'classic' })],
+export default defineConfig(({ mode }) => ({
+	plugins: mode === 'development' ? [react()] : [],
+	esbuild: {
+		jsxInject: `import * as React from 'react'`,
+	},
 	define: {
 		'process.env': {},
 	},
-});
+}));
